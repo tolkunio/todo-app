910f4f06be9c2b890525b322b210f0e1193815dc
Omurbekova Tolkun <tolkun3922@gmail.com>
2021-12-28 14:59:55 +0600

    - display categories

--- todo-app/src/app/views/categories/categories.component.ts
+++ todo-app/src/app/views/categories/categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Category } from 'src/app/model/Category';
+import { DataHandlerService } from 'src/app/service/data-handler.service';
 
 @Component({
   selector: 'app-categories',
@@ -6,10 +8,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.css']
 })
 export class CategoriesComponent implements OnInit {
+  categories:Category[];
 
-  constructor() { }
+  constructor(private dataHandler: DataHandlerService) {
+    this.categories =[];
+   }
 
   ngOnInit(): void {
+  this.categories= this.dataHandler.getCategories();
+  console.log(this.categories);
   }
 
 }
