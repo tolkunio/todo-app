f8667763b365d8037d4485f90724db61268eb666
Omurbekova Tolkun <tolkun3922@gmail.com>
2022-01-13 16:33:11 +0600

    -modified strictpropertyinitialization and added fielsd in task component

--- todo-app/src/app/views/tasks/tasks.component.ts
+++ todo-app/src/app/views/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Task } from 'src/app/model/Task';
 import { DataHandlerService } from 'src/app/service/data-handler.service';
 
@@ -9,15 +10,31 @@ import { DataHandlerService } from 'src/app/service/data-handler.service';
 })
 export class TasksComponent implements OnInit {
   tasks: Task[];
+  public displayedColumns:string[]=['color','id','title','date','priority','category'];
+  public dataSource:MatTableDataSource<Task>;
+
   constructor(private dataHandler: DataHandlerService) {
     this.tasks=[];
    }
 
   ngOnInit(): void {
     this.dataHandler.tasksSubject.subscribe(values => this.tasks = values);
+    this.dataSource = new MatTableDataSource();
+    this.refreshTable();
   }
-  toggleTaskCompleted(task:Task){
+  private refreshTable(){
+    this.dataSource.data = this.tasks; 
+  }
+
+  toggleTaskCompleted(task:Task):void{
     task.isCompleted = !task.isCompleted;
+  }
+  
+  public getPriorityColor(task: Task) : string{
+    if(task.priority && task.priority.color){
+      return task.priority.color;
+    }
+    return '#fff';
   }
 
 }
